7120e5310770eab7f208501d68166a1f2001f194
Tait Hoyem <tait@tait.tech>
2021-11-20 06:51:33 -0700

    working with key vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
 #[macro_use]
 extern crate lazy_static;
 
+use tokio::task;
+use tokio::sync::Mutex;
+use std::ops::{Deref, DerefMut};
+use std::pin::Pin;
+use std::mem::drop;
 use std::any::type_name;
 use std::future::{Future};
 use std::collections::{BTreeSet};
-use std::sync::{Arc,Mutex};
+use std::sync::{Arc};
 use rdev::{
   grab_async,
   Event,
@@ -18,91 +23,42 @@ fn type_of<T>(_: T) -> &'static str {
       type_name::<T>()
 }
 
-#[derive(Copy, Clone)]
-pub struct Modifiers {
-  pub caps_lock: bool,
-  pub l_shift: bool,
-  pub l_alt: bool,
-  pub l_ctrl: bool,
-  pub l_meta: bool,
-  pub r_shift: bool,
-  pub r_alt: bool,
-  pub r_ctrl: bool,
-  pub r_meta: bool,
-}
-
-impl Modifiers {
-  fn new() -> Modifiers {
-    Modifiers {
-      caps_lock: false,
-      l_shift: false,
-      l_alt: false,
-      l_ctrl: false,
-      l_meta: false,
-      r_shift: false,
-      r_alt: false,
-      r_ctrl: false,
-      r_meta: false,
-    }
-  }
-}
-
-type AsyncFn = Box<dyn Fn(Event) -> Box<dyn Future<Output = Option<Event>>> + Send>;
+type Part = Box<dyn Future<Output=Option<bool>>>;
+type AsyncFn = Box<dyn Fn(Vec<Key>) -> Box<dyn Future<Output = bool> + Unpin + Send + 'static> + Send + 'static>;
 
 lazy_static! {
-  static ref CURRENT_MODIFIERS: Arc<Mutex<Modifiers>> = Arc::new(Mutex::new(Modifiers::new()));
+  static ref CURRENT_KEYS: tokio::sync::Mutex<Vec<Key>> = Mutex::const_new(Vec::new());
+   static ref TEST: tokio::sync::Mutex<AsyncFn> = tokio::sync::Mutex::const_new(
+Box::new(move |keys: Vec<Key>| {
+  Box::new(Box::pin(async move {
+    false
+  }))
+}));
 }
-static TEST: Arc<Mutex<AsyncFn>> = Arc::new(Mutex::new(Box::new(|ev: Event| {
-  Box::new(
-    Box::new(|| async move{
-      Some(ev)
-    })()
-  )
-})));
 
 
-async fn internal_listener(event: Event) -> Option<Event>{
-    let aclone = Arc::clone(&CURRENT_MODIFIERS);
+async fn internal_listener(event: Event) -> Option<Event> {
     match event.event_type {
       KeyPress(x) => {
-        if let Ok(mut mods) = aclone.lock() {
-            match x {
-              Key::CapsLock => mods.caps_lock = true,
-              Key::ControlLeft => mods.l_ctrl = true,
-              Key::ControlRight => mods.r_ctrl = true,
-              Key::Alt => mods.l_alt = true,
-              Key::AltGr => mods.r_alt = true,
-              Key::MetaLeft => mods.l_meta = true,
-              Key::MetaRight => mods.r_meta = true,
-              Key::ShiftLeft => mods.l_shift = true,
-              Key::ShiftRight => mods.r_shift = true,
-              _ => {}
-            }
-          }
-          let consume: Box<dyn Future<Output=bool>> = Box::new(async move {
-            true
-          });
-          if consume.unpin() {
-            return None;
-          } else {
-            return Some(event);
-          }
+        let mut mods = CURRENT_KEYS.lock().await;
+         mods.push(x);
+
+            let sf = TEST.lock().await;
+                mods.dedup();
+                let consume: task::JoinHandle<bool> = task::spawn(sf(mods.clone()));
+                println!("TYPE: {:?}", type_of(consume.await.unwrap()));
+                /*
+                if consume.await? {
+                  return None;
+                } else {
+                  return Some(event);
+                }*/
+            
+          
       }
       KeyRelease(x) => {
-        if let Ok(mut mods) = aclone.lock() {
-        match x {
-            Key::CapsLock => mods.caps_lock = false,
-            Key::ControlLeft => mods.l_ctrl = false,
-            Key::ControlRight => mods.r_ctrl = false,
-            Key::Alt => mods.l_alt = false,
-            Key::AltGr => mods.r_alt = false,
-            Key::MetaLeft => mods.l_meta = false,
-            Key::MetaRight => mods.r_meta = false,
-            Key::ShiftLeft => mods.l_shift = false,
-            Key::ShiftRight => mods.r_shift = false,
-            _ => {}
-          }
-        } 
+        let mut mods = CURRENT_KEYS.lock().await;
+        mods.retain(|&k| k != x);
       }
     _ => ()
   }
@@ -112,12 +68,13 @@ async fn internal_listener(event: Event) -> Option<Event>{
 
 pub async fn initialize_key_register<F, T>(callback: F) 
 where
-  F: Fn(Modifiers, Key) -> T + Send + 'static,
-  T: Future<Output=bool> + Send + 'static,
+  F: Fn(Vec<Key>) -> T + Send + 'static,
+  T: Future<Output=bool> + Send  + 'static,
 {
-  if let Ok(tf) = TEST.lock() {
-    *tf = callback;
-  }
+  let mut tf = TEST.lock().await;
+  *tf = Box::new(move |keys: Vec<Key>| {
+      Box::new(Box::pin(callback(keys)))
+    });
   // this will spawn a new thread, which calls internal listener whenever a key is pressed.
   if let Err(error) = grab_async(internal_listener).await {
     panic!("Error with key handler: {:?}", error);
