694259ad07eee91a168e3c695f72a713f14dd1ee
Michael Connor Buchan <mikey@blindcomputing.org>
2021-11-21 17:23:15 +0000

    Completely rework to use a thread for input, which sends events down a tokio channel

--- src/lib.rs
+++ src/lib.rs
@@ -1,98 +1,26 @@
-#[macro_use]
-extern crate lazy_static;
+use tokio::sync::mpsc;
 
-use tokio::task;
-use tokio::sync::Mutex;
-use std::ops::{Deref, DerefMut};
-use std::pin::Pin;
-use std::mem::drop;
-use std::any::type_name;
-use std::future::{Future};
-use std::collections::{BTreeSet};
-use std::sync::{Arc};
-use rdev::{
-  grab_async,
-  Event,
-  EventType,
-  EventType::{KeyPress, KeyRelease},
-  Key
-};
+use once_cell::sync::OnceCell;
 
-fn vector_equals(va: &Vec<Key>, vb: &Vec<Key>) -> bool{
-  (va.len() == vb.len()) &&  // zip stops at the shortest
-    va.iter()
-    .zip(vb)
-    // TODO: very slow
-    .all(|(a,b)| format!("{:?}", a) == format!("{:?}", b))
+const MAX_EVENTS: usize = 256;
+
+thread_local! {
+    static TX: OnceCell<mpsc::Sender<rdev::Event>> = OnceCell::new();
 }
 
-fn type_of<T>(_: T) -> &'static str {
-      type_name::<T>()
-}
-
-type Part = Box<dyn Future<Output=Option<bool>>>;
-type AsyncFn = Box<dyn Fn(Vec<Key>) -> Box<dyn Future<Output = bool> + Unpin + Send + 'static> + Send + 'static>;
-
-lazy_static! {
-  static ref LAST_KEYS: tokio::sync::Mutex<Vec<Key>> = Mutex::const_new(Vec::new());
-  static ref CURRENT_KEYS: tokio::sync::Mutex<Vec<Key>> = Mutex::const_new(Vec::new());
-   static ref TEST: tokio::sync::Mutex<AsyncFn> = tokio::sync::Mutex::const_new(
-Box::new(move |keys: Vec<Key>| {
-  Box::new(Box::pin(async move {
-    false
-  }))
-}));
-}
-
-
-async fn internal_listener(event: Event) -> Option<Event> {
-    match event.event_type {
-      KeyPress(x) => {
-        let mut mods = CURRENT_KEYS.lock().await;
-          let mut last_keys = LAST_KEYS.lock().await;
-          *last_keys = mods.clone();
-         mods.push(x);
-            let sf = TEST.lock().await;
-                mods.dedup();
-                //println!("CURR[D]: {:?}", mods);
-                //println!("LAST[D]: {:?}", last_keys);
-                if !vector_equals(&mods, &last_keys) {
-                  let consume: task::JoinHandle<bool> = task::spawn(sf(mods.clone()));
-                  //println!("TYPE: {:?}", type_of(consume.await.unwrap()));
-                  if consume.await.unwrap() {
-                    return None;
-                  } else {
-                    return Some(event);
-                  }
-                } 
-            
-          
-      }
-      KeyRelease(x) => {
-        let mut last_keys = LAST_KEYS.lock().await;
-        let mut mods = CURRENT_KEYS.lock().await;
-        *last_keys = mods.clone();
-        mods.retain(|&k| k != x);
-                //println!("CURR[U]: {:?}", mods);
-                //println!("LAST[U]: {:?}", last_keys);
-      }
-    _ => ()
-  }
-  Some(event)
-}
-
-
-pub async fn initialize_key_register<F, T>(callback: F) 
-where
-  F: Fn(Vec<Key>) -> T + Send + 'static,
-  T: Future<Output=bool> + Send  + 'static,
-{
-  let mut tf = TEST.lock().await;
-  *tf = Box::new(move |keys: Vec<Key>| {
-      Box::new(Box::pin(callback(keys)))
+pub fn init() -> mpsc::Receiver<rdev::Event> {
+    let (tx, rx) = mpsc::channel(MAX_EVENTS);
+    std::thread::spawn(move || {
+        TX.with(|global| global.set(tx).unwrap());
+        rdev::grab(|ev| {
+            TX.with(|tx| {
+                let tx = tx.get().unwrap();
+                if let Err(e) = tx.blocking_send(ev.clone()) {
+                    eprintln!("Warning: Failed to process key event: {}", e);
+                }
+            });
+            Some(ev)
+        })
     });
-  // this will spawn a new thread, which calls internal listener whenever a key is pressed.
-  if let Err(error) = grab_async(internal_listener).await {
-    panic!("Error with key handler: {:?}", error);
-  }
+    rx
 }
